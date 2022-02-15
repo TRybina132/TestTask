42e573345cffc2e7d71aad7b3cad0719c386612b
TRybina132 <98330987+TRybina132@users.noreply.github.com>
2022-02-15 19:09:11 +0200

    Add files via upload

---
+++ TestTasks/TestTasks/ArrayOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTasks
+{
+    public class ArrayOperations
+    {
+        public static int GetDigits(int number) => Convert.ToInt32(Math.Floor(Math.Log10(Math.Abs(number)) + 1));
+        public static void CheckIfMin(ref int min, int current) => min = current < min ? current : min;
+        public static void CheckIfMax(ref int max, int current) => max = current > max ? current : max;
+        public static void Process(int[] arr)
+        {
+            int sumA = 0;
+            int minA = arr[0], maxA = arr[0];
+            arr.ToList().ForEach(num => 
+            {
+                switch (GetDigits(num))
+                {
+                    case 1: CheckIfMin(ref minA, num); break;
+                    case 2: sumA += num; break;
+                    case 3: CheckIfMax(ref maxA, num); break;
+                }
+            });
+
+            Console.Write($"min value of one-digit number: {minA}\n"
+                + $"Sum of two-digits numbers: {sumA} \n" 
+                + $"max value of three-digit number: {maxA}");
+        }
+    }
+}
